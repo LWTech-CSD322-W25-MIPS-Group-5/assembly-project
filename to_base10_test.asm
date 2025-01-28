cdc3a9642ed111ed0a7d4f9086553c2f55a96161
Daniel McCord <daniel.mccord@lwtech.edu>
2025-01-27 20:32:13 -0800

    Add new test cases for ToBase10

--- to_base10_test.asm
+++ to_base10_test.asm
@@ -8,18 +8,27 @@
 
 .data
 # Test cases:
-# a6 in base 16 should be 166
-# 1010 in base 2 should be 10
-# zzz in base 36 should be 46655
+# "a6" in base 16 should be 166
+# "1010" in base 2 should be 10
+# "zzz" in base 36 should be 46655
+# "0" in base 1 should be -1
+# "aa" in base 10 should be -1
+# "aa" in base 11 should be 120
+# "" in base 2 should be 0
+# ":" in base 10 should be -1
+# ":" in base 11 should be -1
+# "z" in base 37 should be -1
+# "{" in base 36 should be -1
+# "9" in base 0x7FFFFFFE should be -1
 #
-# The end of the test cases is denoted by sentinel values (e.g. -1 for in_bases).
+# The end of the test cases is denoted by a sentinel value (i.e. -1 for in_bases).
 #
-# TODO: feel free to add more test cases as you see fit.
+# Feel free to add more test cases as you see fit.
 
-in_bases: .word 16, 2, 36, -1
-in_numbers: .asciiz "a6", "1010", "zzz"
+in_bases: .word 16, 2, 36, 1, 10, 11, 2, 10, 11, 37, 36, 0x7FFFFFFF, -1
+in_numbers: .asciiz "a6", "1010", "zzz", "0", "aa", "aa", "", ":", ":", "z", "{", "9"
 .align 2  # align the out_numbers on a word boundary (after the asciiz data)
-out_numbers: .word 166, 10, 46655
+out_numbers: .word 166, 10, 46655, -1, -1, 120, 0, -1, -1, -1, -1, -1
 fail: .asciiz "A test failed\n"
 success: .asciiz "All tests passed\n"
 
